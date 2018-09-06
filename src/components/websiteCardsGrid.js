5b785bbdcde8491f0d680dbb7c4fd3ae2b402e85
Alex Tishkov <techno199@live.ru>
2018-09-06 21:16:04 +0300

    hotfix

--- src/components/websiteCardsGrid.js
+++ src/components/websiteCardsGrid.js
@@ -13,6 +13,9 @@ const styles = theme => ({
   },
   titleWrapper: {
     marginBottom: 37
+  },
+  websitesWrapper: {
+    marginBottom: 92
   }
 });
 
@@ -20,34 +23,32 @@ class WebsiteCardsGrid extends Component {
   render() {
     const { classes, location } = this.props;
     return (
-      <div>
-        <Grid container justify='center' wrap='wrap'>
-          <Grid item xs={11} lg={8} className={classes.titleWrapper}>
-            <Typography className={classes.title}>
-              {location}
-            </Typography>
-          </Grid>
-          <Grid item xs={11} lg={8}>
-            <Grid container spacing={24} className={classes.websitesWrapper}>
-              <Grid item>
-                <CreateWebsiteCard />
-              </Grid>
-              <Grid item>
-                <WebsiteCard />
-              </Grid>
-              <Grid item>
-                <WebsiteCard />
-              </Grid>
-              <Grid item>
-                <WebsiteCard />
-              </Grid>
-              <Grid item>
-                <WebsiteCard />
-              </Grid>
+      <Grid container justify='center' wrap='wrap'>
+        <Grid item xs={11} lg={8} className={classes.titleWrapper}>
+          <Typography className={classes.title}>
+            {location}
+          </Typography>
+        </Grid>
+        <Grid item xs={11} lg={8}>
+          <Grid container spacing={24} className={classes.websitesWrapper}>
+            <Grid item>
+              <CreateWebsiteCard />
+            </Grid>
+            <Grid item>
+              <WebsiteCard />
+            </Grid>
+            <Grid item>
+              <WebsiteCard />
+            </Grid>
+            <Grid item>
+              <WebsiteCard />
+            </Grid>
+            <Grid item>
+              <WebsiteCard />
             </Grid>
           </Grid>
         </Grid>
-      </div>
+      </Grid>
     )
   }
 }
