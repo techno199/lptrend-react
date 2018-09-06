c5b7c40c31802d1360b6fdd9b136a25e8b297984
Alex Tishkov <techno199@live.ru>
2018-09-06 21:02:47 +0300

    warm flow

--- src/components/appBar.js
+++ src/components/appBar.js
@@ -100,8 +100,8 @@ export class CustomAppBar extends Component {
                 open={open}
                 onClose={this.handleClose}
               >
-                <MenuItem onClick={this.handleClose}>Profile</MenuItem>
-                <MenuItem onClick={this.handleClose}>My account</MenuItem>
+                <MenuItem onClick={this.handleClose}>React is great</MenuItem>
+                <MenuItem onClick={this.handleClose}>You are great too</MenuItem>
               </Menu>
             </div>
           </Toolbar>
